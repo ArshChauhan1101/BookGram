c054137a994bc0d0b3f259693c79cc29874b517e
ArshChauhan1101 <achauhan4830@conestogac.on.ca>
2023-11-05 13:31:57 -0500

    Code Before React Error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,41 @@
-import React, {useState, useSyncExternalStore} from "react";
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import "./Navbar.css";
-import logoImg from "../../images/bookgram.png"
-import {HiOutlineMenualt3} from "react-icons/hi";
+import logoImg from "../../images/bookgram.png";
+import {HiOutlineMenuAlt3} from "react-icons/hi";
 
-const Navbar = () => 
-{
-const [toggleMenu, SetToggleMenu] = useState(false);
-const handleNavbar = () => SetToggleMenu (!toggleMenu);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleNavbar = () => setToggleMenu(!toggleMenu);
 
-    return (
-        <nav className= 'navbar' id ="navbar">
-            <div className='container navbar-content flex'>
-                <div className = 'brand-and-toffler flex-sb'>
-                    <Link to = "/" className = 'navbar-brand flex>'>
-                    <img src = {logoImg} alt = "site logo" />
-                    <span className = 'text-uppercase fw-7 fs-24 ls-1'>BookGram</span>
-                    </Link>
-                    <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
-                        <HiOutlineMenualt3 size = {35} style = {{color: '${toggleMenu ? "#'}}
-                    </button>
-                </div>
-            </div>
-        </nav>
+  return (
+    <nav className='navbar' id = "navbar">
+      <div className='container navbar-content flex'>
+        <div className='brand-and-toggler flex flex-sb'>
+          <Link to = "/" className='navbar-brand flex'>
+            <img src = {logoImg} alt = "site logo" style={{ marginRight: '20px' }}/>
+            <span className='text-uppercase fw-7 fs-24 ls-1'>BookGram</span>
+          </Link>
+          <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
+            <HiOutlineMenuAlt3 size = {35} style = {{
+              color: `${toggleMenu ? "#fff" : "#010101"}`
+            }} />
+          </button>
+        </div>
 
-    )
+        <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collapse"}>
+          <ul className = "navbar-nav">
+            <li className='nav-item'>
+              <Link to = "book" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to = "about" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>about</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import {FaSearch} from "react-icons/da";
+/*import { useNavigate } from "react-router-dom";*/
+import "./SearchForm.css";
 
 const SearchForm = () => {
     return (
-        <div>
-            Header
+        <div className= 'search-form'>
+            <div className='container'>
+                <div className='search-form-content'>
+                    <form className= 'search-form'>
+                        <div className='searchform-form-elem flex flex-sb bg-white' >
+                            <input type = "text"
+                            className='form-control' placeholder='The Lost world..' />
+                            <button type = "submit" className='flex flex -c'>
+                                <FaSearch className = 'text-brown' size = {32}/>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
